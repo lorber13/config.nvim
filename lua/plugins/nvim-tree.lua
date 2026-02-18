d25d22b7d44f174ea08b1e4040531a9409fad38a
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2026-02-18 21:40:30 +0100

    feat: add tree file picker

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    actions = {
+      open_file = {
+        quit_on_open = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      "<cmd>NvimTreeToggle<cr>",
+      desc = "File picker",
+    },
+  },
+}
