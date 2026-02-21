2f579eb860af31242d6bcfb2540de4f79101c5bc
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2026-02-21 20:35:26 +0100

    fix: add keyboard shortcuts for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,4 +6,16 @@ return {
     -- optional but recommended
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
+  keys = {
+    {
+      "<leader>s",
+      "<cmd>Telescope find_files<cr>",
+      desc = "Find files",
+    },
+    {
+      "<leader><leader>",
+      "<cmd>Telescope live_grep<cr>",
+      desc = "Search everywhere",
+    },
+  },
 }
