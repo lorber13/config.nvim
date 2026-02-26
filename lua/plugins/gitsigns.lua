17b5d647989bc3c8a8126f5e8a105e064454449c
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2026-02-26 22:52:56 +0100

    feat: add git line blame and customize signs

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,14 @@
 return {
   "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+    },
+    signs_staged = {
+      add = { text = "+" },
+      change = { text = "~" },
+    },
+  },
 }
